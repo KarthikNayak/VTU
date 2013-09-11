10f59851d1ced784aacb18c7df640f01734b8831
Karthik Nayak <karthik.188@gmail.com>
2013-09-12 00:11:35 +0530

    Documentation Complete

--- CSE/3rdSem/DataStructures/LabProg3.c
+++ CSE/3rdSem/DataStructures/LabProg3.c
@@ -8,7 +8,7 @@
 #include <ctype.h>      //For String Length 
 //Global Variables
 
-int s[20], top = 0;		//s[20] stores the characters, acts like a temporary storage
+int s[20], top = 0;		//s[20] stores the characters, saves the final reult 
 
 //push function takes characters and stores them in global array s temporarily to evaluvate the //term
 
@@ -18,7 +18,7 @@ void push(int elem)
 	top++;
 }
 
-//pop function
+//pop function this is used to get the element on top of s[]
 int pop() 
 {
 	top--;
@@ -27,22 +27,22 @@ int pop()
 
 void main() 
 {
-	char postfix[20], ch;
-	int i, op1, op2, res;
+	char postfix[20], ch;	//postfix is used to get input and ch is used for checking each character
+	int i, op1, op2, res;	//i is for the loop op1 op2 and res is for calculations
 	clrscr();
 	printf(“Postfix Expression Evaluvation\n”);
 	printf(“Enter the expression : \n”);
-	scanf(“ % s”, postfix);
-	for (i = 0; i < strlen(postfix); i++) 
+	scanf(“ % s”, postfix);		//expression is obtained
+	for (i = 0; i < strlen(postfix); i++)  //loop to check each character till end of expression
 	{
-		ch = postfix[i];
-		if (isdigit(ch))
-		push(ch - ’0’)
-		else
+		ch = postfix[i];	//ch is set to i'th element of postfix ,starting from 0
+		if (isdigit(ch))	//if its a digit it needs to be stored hence the check
+		push(ch - ’0’)	//push is used to send the digit to s[] array and we negate 0 to make it an integer and convert from char to int
+		else	//this states that there is an operation 
 		{
-			op2 = pop();
+			op2 = pop();		//so we need to get the last two digits to operate on hence pop is used 
 			op1 = pop();
-			switch (ch)
+			switch (ch) 	//depending on the operator , the operation is performed
 			{
 				case ‘+’: res = op1 + op2; break;
 				case ‘-’: res = op1 - op2; break;
@@ -51,9 +51,9 @@ void main()
 				case ‘^’: res = pow(op1, op2); break;
 				default : printf(“Invalid Entry”);
 			}
-			push(res);
+			push(res); 	//now we need to push the result obtained to the top of s
 		}
 	}
-	printf(“Result expression : % d”, pop());
+	printf(“Result expression : % d”, pop()); // here we print the final value . which is the top of s 
 	getch();
 }
