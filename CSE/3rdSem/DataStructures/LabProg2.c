6cc5fdc4652e6a0a7074533826f3f63f3c81235d
Karthik Nayak <karthik.188@gmail.com>
2013-09-12 19:06:35 +0530

    Finish prog2 and fixed erros

--- CSE/3rdSem/DataStructures/LabProg2.c
+++ CSE/3rdSem/DataStructures/LabProg2.c
@@ -2,32 +2,43 @@
 characters are taken as input and we assume that each character is postive*/
 
 //Header Files
-#include <conio.h>      //For Clear Screen Function
-#include <stdio.h>      //For Std Input and Output
-#include <ctype.h>      //For String Length 
-#include <stdlib.h> 	
+#include <stdio.h>
+//For Std Input and Output
+#include <ctype.h>   
+//For string length    
+#include <string.h>
+//For checking wether alphabet/numerical		
 
 //Pre-pocessor derivatives
-
-#define MAX 20;
+//Here we take twenty to be the maximum size of input taken
+#define MAX 20		
 
 //Global Variables
 
 char s[MAX];
+/*Here array S is taken as a temporary array and we use to 
+check the precedence of operators to basically know where each operator
+must be place*/
 int top=0;
+/*top is used to keep the position of the last operator sent 
+to the array s */
 
+
+/*Push is used to push operators to the array s */
 void push(char elem) 
 {
 	top++;
 	s[top] = elem;
 }
 
+/*pop is used to get the last operator inserted to the array s*/
 int pop() 
 {
-	top--;
-	return s[top];
+	return s[top--];
 }
 
+/*precedence is used to check the precedence of the operators being checked
+notice that # is given 0 becasue it the the default value sent to s*/
 int precedence(char elem)
 {
 	switch(elem)
@@ -45,33 +56,48 @@ int precedence(char elem)
 void main()
 {
 	char infix[MAX],postfix[MAX],ch;
+	/*infix=input postfix=result to be obtained 
+	ch=variable to keep each element of input*/
 	int i=0,j=0;
-	clrscr();
 	printf("Enter the infix expression\n");
-	scanf("%s,&infix");
+	scanf("%s",infix);
 	push('#');
-	for(i=0;i<strlen(infix);i++)
+	/*we push # as we have given it the lowest value
+	which I mention before in precedence*/
+	//loop to go through each element of input
+	for(i=0;i<strlen(infix);i++) 
 	{
 		ch=infix[i];
-		if(isalnum(ch))
+		//ch is given the i'th value of infix
+		//if alphabet/number directly send it to postfix
+		if(isalnum(ch))		
 			postfix[j++]=ch;
-		else if(ch=='(')
+		/*if opening bracket send it to array s 
+		as opening bracket has highest importance*/
+		else if(ch=='(')	
 			push(ch);
+		/*if closing braket everything in s has to sent 
+		to the postfix so till opening bracket is found*/
 		else if(ch==')')
 		{
 			while(s[top]!='(')
 				postfix[j++]=pop();
+				pop();
 		}
+		/*if just an operator we check it with the last operator
+		and the operator with higher precedence is sent to postfix
+		and ch is sent to array s*/
 		else
 		{
-			while(precedence(s[top]>=precedence(ch)))
+			while(precedence(s[top])>=precedence(ch))
 				postfix[j++]=pop();
-			push(ch);		
+			push(ch);
 		}
 	}
+	/*remaining operator is sent to postfix*/
 	while(s[top]!='#')
 		postfix[j++]=pop();
 	postfix[j]='\0';
-	printf("postfix expression conversion is %s\n",postfix );
-	getch();
+	/*end postfix and print it*/
+	printf("postfix expression conversion is %s\n",postfix);
 }
