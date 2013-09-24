46edc553c453caa187494b2cabd6b6fa24780194
Nitesh A Jain <niteshjain92@gmail.com>
2013-09-24 23:19:18 +0530

    typo in folder name

---
+++ CSE/6thSem/unix-system-programming-lab/5a.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<unistd.h>
+int main(int argc,char *argv[])
+{ 
+int i;
+char **ptr;
+extern char **environ;
+for(i=0;i<argc;i++)
+printf("argv[%d]:%s\n",i,argv[i]);
+for(ptr=environ;*ptr!=0;ptr++)
+printf("%s\n",*ptr);
+return 0;
+}
