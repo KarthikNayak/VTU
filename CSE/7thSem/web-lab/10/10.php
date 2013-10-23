fe11f63fca7b2308c694fd226ccbf7dfc5d33bbf
Nitesh A Jain <niteshjain92@gmail.com>
2013-10-23 21:44:39 +0530

    modified to work with modern browsers

--- CSE/7thSem/web-lab/10/10.php
+++ CSE/7thSem/web-lab/10/10.php
@@ -1,12 +1,20 @@
 <?php
 session_start();
-session_register("count");
-if (!isset($_SESSION))
-{
-$_SESSION["count"] = 0;
-echo "<p>Counter initialized</p>\n";
-}
-else { $_SESSION["count"]++; }
-echo "<p>The counter is now <b>$_SESSION[count]</b></p><p>reload this page to increment</p>";
+// store session data
+if(!isset($_SESSION))
+$_SESSION['views']=1;
+else
+$_SESSION['views']++;
+
 ?>
 
+<html>
+<body>
+
+<?php
+//retrieve session data
+echo "Pageviews=". $_SESSION['views'];
+?>
+
+</body>
+</html>
