f9d209fc87d8fd324d1fb1e681a7fe5c8a2400c3
Nitesh A Jain <niteshjain92@gmail.com>
2013-10-30 00:49:47 +0530

    some restructuring

--- CSE/7thSem/web-lab/12/README.md
+++
@@ -1,3 +0,0 @@
-A simple Ruby on Rails app for a simple library book management. One can add a book or query for a book
-
-
--- CSE/7thSem/web-lab/12/app/controllers/books_controller.rb
+++ CSE/7thSem/web-lab/12/app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   def index
   end
   def add
-	Book.create(:ano=> params[:b_ano],:title=> params[:b_title],:author=> params[:b_author],:edition=> params[:b_edition],:publisher=> params[:b_publisher])
+	Book.create(:access_no=> params[:b_access_no],:title=> params[:b_title],:author=> params[:b_author],:edition=> params[:b_edition],:publisher=> params[:b_publisher])
 	redirect_to :action => 'index'
   end
   def search
