f88d7525bb4c9f3dcc3b828177cf6e27c869a6fb
Karthik Nayak <karthik.188@gmail.com>
2014-10-29 23:04:45 +0530

    Update SS Lab Q8.a to take arguments
    Signed-off-by: Karthik Nayak <karthik.188@gmail.com>

--- CSE/5thSem/SSLab/8a.sh
+++ CSE/5thSem/SSLab/8a.sh
@@ -1,11 +1,12 @@
-echo "Enter the two filenames"
-read file1
-read file2
-perm1=`ls -l $file1 | gawk '{ print $1}'`
-perm2=`ls -l $file2 | gawk '{ print $1}'`
+if [ $# -ne 2 ]; then
+    echo "arguments invalid"
+    exit
+fi
+perm1=`ls -l $1 | gawk '{ print $1}'`
+perm2=`ls -l $2 | gawk '{ print $1}'`
 if [ "$perm1" == "$perm2" ]; then
     echo "$perm1"
 else
-    echo "File : $file1, Permission : $perm1"
-    echo "File : $file2, Permission : $perm2"
+    echo "File : $1, Permission : $perm1"
+    echo "File : $2, Permission : $perm2"
 fi
